96a2fd695c5866b0aa2550c5647c45e76a717dcd
spystar20 <hert517@gmail.com>
2026-02-16 18:22:39 +0530

    Initial Vue + Tailwind setup

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-black w-full px-5 py-10 text-lg font-semibold ">
+        <ul class="flex justify-center items-center gap-10">
+            <li class="px-5 py-1 border border-gray-700 rounded-full hover:bg-black hover:text-white hover:-translate-y-1 hover:ease-in hover:duration-150 cursor-pointer "><a>Home</a></li>
+                <li class="px-5 py-1 active:border border-gray-700 rounded-full hover:bg-black hover:text-white hover:-translate-y-1 hover:ease-in hover:duration-150 cursor-pointer "><a>about</a></li>
+                  <li class="px-5 py-1 border border-gray-700 rounded-full hover:bg-black hover:text-white hover:-translate-y-1 hover:ease-in hover:duration-150 cursor-pointer "><a>services</a></li>
+                      <li class="px-5 py-1 border border-gray-700 rounded-full hover:bg-black hover:text-white hover:-translate-y-1 hover:ease-in hover:duration-150 cursor-pointer "><a>events</a></li>
+                          <li class="px-5 py-1 border border-gray-700 rounded-full hover:bg-black hover:text-white hover:-translate-y-1 hover:ease-in hover:duration-150 cursor-pointer "><a>contact</a></li>
+        </ul>
+        <div>
+            <button class="bg-black text-white ">
+                Book Now
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
