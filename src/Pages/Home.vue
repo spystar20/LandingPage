7171d1843a82a35709695f1e406293e04e87023b
spystar20 <hert5197@gmail.com>
2026-03-26 17:53:32 +0530

    hover fixed

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -26,9 +26,9 @@
         <!-- about -->
         <div class="py-5 flex flex-row gap-8">
             <!-- col-1 -->
-            <div class="w-[70%]">
+            <div class="w-[70%] ">
                 <div style="background-image: url(https://i.pinimg.com/1200x/5d/2f/b8/5d2fb87f8c6aacb1d930e01e401f8a05.jpg
-);" class="bg-black rounded-4xl bg-center bg-cover h-screen flex flex-col items-start  justify-end">
+);" class="bg-black rounded-4xl bg-center bg-cover  h-screen flex flex-col items-start  justify-end ">
                     <div
                         class="flex flex-col items-start justify-start rounded-4xl text-black font-poppins capitalize ">
                         <span class="pr-6 pt-4 rounded-tr-lg  bg-white">
@@ -67,17 +67,17 @@
         </div>
 
         <!-- project -->
-        <div class="py-5  ">
+        <div class="py-16">
             <!-- heading -->
             <div class="flex flex-row  justify-between items-start w-full">
-                <div class="w-full">
+                <div class="w-[50%]">
                     <h2
                         class="font-bold font-heading text-5xl bg-linear-to-r from-black to-main text-transparent bg-clip-text ">
                         Explore Our Collection
                     </h2>
 
                 </div>
-                <div class="flex flex-col items-end justify-center gap-4">
+                <div class="w-[40%]">
                     <p class="text-end font-body font-medium text-sm">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum possimus atque dolores quasi
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. A
@@ -87,20 +87,20 @@
             </div>
             <div class="grid grid-cols-4 h-screen">
 
-                <div class="col-span-1  relative">
+                <div class="col-span-1  relative group overflow-hidden rounded-3xl">
                     <div style="background-image: url(https://i.pinimg.com/1200x/ac/61/19/ac61196521cec3df1e94987c0b176b79.jpg);"
-                        class="w-full bg-cover rounded-2xl h-[70%]">
+                        class="w-full bg-cover bg-center rounded-3xl h-100 group-hover:scale-105 transition transform duration-500 ease-in">
 
                     </div>
                     <div class="h-[70%] w-full rounded-2xl bg-linear-to-t from-black/90 from-0% via-black/40 via-25% to-transparent to-30% absolute inset-0 flex items-end justify-between px-3 py-6">
-                        <div class="w-full flex flex-col gap-4 group">
+                       <div class="transform translate-y-10 group-hover:translate-y-0 transition duration-300">
                         <div class="flex  justify-between items-center ">
                             <h1 class="text-2xl font-heading font-semibold  text-white">Property A</h1>
                             <span class="  text-black bg-white rounded-full p-2 ">
                                 <ArrowUpRight class="hover:scale-125 transition-all ease-in duration-200 cursor-pointer " />
                             </span>
                         </div>
-                        <div class=" hidden group-hover:flex group-hover:flex-col gap-6 ">
+                      <div class="opacity-0 group-hover:opacity-100 transition duration-300 mt-3">
 <p class="font-body text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque magni alias ipsam?</p>
 <div class="flex items-center justify-center  gap-3 w-full">
     <button class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">view</button>
@@ -113,6 +113,7 @@
                 </div>
             </div>
         </div>
+   
     </div>
 </template>
 
