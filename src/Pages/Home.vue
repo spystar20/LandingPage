dbbd14cbb0b03f8649cabdcd299d14b0e6446bf7
spystar20 <hert5179@gmail.com>
2026-05-07 20:57:46 +0530

    prop

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -3,6 +3,7 @@ import { ArrowUpRight,ArrowLeft, PenTool } from 'lucide-vue-next';
 import approach from '../assets/approach.png'
 import gsap from 'gsap';
 import { onMounted } from 'vue';
+import ProjectCard from '../components/ProjectCard.vue';
 
 onMounted(() => {
     gsap.from(".hero-title", {
@@ -190,38 +191,7 @@ const Col1 = [
 
             <div class="grid grid-cols-4 gap-5 py-10 w-full min-h-screen">
                 <div  class="flex flex-col h-full gap-3 ">
-                    <div :key="item.id" v-for="item in Col1" :style="{height:item.height}" class="   relative group overflow-hidden rounded-3xl">
-                        <div :style='{backgroundImage: `url(${item.img})`}'
-                            class="w-full bg-cover bg-center rounded-3xl h-full group-hover:scale-105 transition transform duration-500 ease-in">
-                        </div>
-                        <div
-                            class=" w-full h-auto hover:bg-black/30 ease-in duration-300 transition-all rounded-2xl bg-linear-to-t from-black/90 from-0% via-black/40 via-25% to-transparent to-30% absolute inset-0 flex items-end justify-between py-3 px-6">
-                            <div class="transform translate-y-[65%] group-hover:translate-y-0 transition duration-300">
-                                <div class="flex  justify-between items-center ">
-                                    <h1 class="text-3xl font-projects font-semibold  text-white">{{ item.title }}</h1>
-                                    <span class="  text-black bg-white rounded-full p-2 ">
-                                        <ArrowUpRight
-                                            class="hover:scale-125 transition-all ease-in duration-200 cursor-pointer" />
-                                    </span>
-                                </div>
-                                <div class="opacity-0 group-hover:opacity-100 transition duration-300 mt-3">
-
-                                    <p class="font-body text-white text-sm py-2 text-center">Lorem ipsum dolor sit amet,
-                                        consectetur
-                                        adipisicing elit. Cumque magni alias ipsam?</p>
-                                    <!-- buttons -->
-                                    <div class="flex items-center justify-center  gap-3 py-2 w-full">
-                                        <button
-                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">view</button>
-                                        <button
-                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">contact</button>
-                                        <button
-                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">learn</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   <ProjectCard v-for="item in Col1" :key="item.id" :project="item"/>
                 </div>
                 <!-- coloumn-2   -->
                 <div class="flex flex-col col-span-2 h-full gap-3 ">
@@ -330,38 +300,9 @@ const Col1 = [
                     </div>
                 </div>
                 <div class="flex flex-col h-full gap-3 ">
-                    <div v-for="item in Col2" :key="item.id" :style="{height:item.height}" class="  relative group overflow-hidden rounded-3xl">
-                        <div :style="{backgroundImage:`url(${item.img})`}"
-                            class="w-full bg-cover bg-center rounded-3xl h-full group-hover:scale-105 transition transform duration-500 ease-in">
-                        </div>
-                        <div
-                            class=" w-full h-auto hover:bg-black/30 ease-in duration-300 transition-all rounded-2xl bg-linear-to-t from-black/90 from-0% via-black/40 via-25% to-transparent to-30% absolute inset-0 flex items-end justify-between py-3 px-6">
-                            <div class="transform translate-y-1/2 group-hover:translate-y-0 transition duration-300">
-                                <div class="flex  justify-between items-center ">
-                                    <h1 class="text-3xl font-projects font-semibold  text-white">{{ item.title }} </h1>
-                                    <span class="  text-black bg-white rounded-full p-2 ">
-                                        <ArrowUpRight
-                                            class="hover:scale-125 transition-all ease-in duration-200 cursor-pointer" />
-                                    </span>
-                                </div>
-                                <div class="opacity-0 group-hover:opacity-100 transition duration-300 mt-3">
+              
+                                   <ProjectCard v-for="item in Col2" :key="item.id" :project="item"/>
 
-                                    <p class="font-body text-white text-sm py-2">Lorem ipsum dolor sit amet, consectetur
-                                        adipisicing elit. Cumque magni alias ipsam?</p>
-                                    <!-- buttons -->
-                                    <div class="flex items-center justify-center  gap-3 py-2 w-full">
-                                        <button
-                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">view</button>
-                                        <button
-                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">contact</button>
-                                        <button
-                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">learn</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                
                 </div>
 
 
@@ -376,7 +317,7 @@ const Col1 = [
     <div class="flex items-center gap-3">
         <PenTool class="text-white w-6 h-6 opacity-80"/>
         <h2 class="text-xl font-semibold ">Bespoke Concepts</h2></div>
-    <P class="text-sm font-body  text-gray-300 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores necessitatibus t Lorem ipsum dolor sit amet.</P>
+    <p class="text-sm font-body  text-gray-300 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores necessitatibus t Lorem ipsum dolor sit amet.</p>
 </div>
 <div class=" border-gray-200/20 border rounded-2xl flex flex-col gap-3 p-5 bg-white/5 backdrop-blur-md transition duration-300 hover:bg-white/10">
     <div class="flex items-center gap-3">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ArrowUpRight } from 'lucide-vue-next';
+
+defineProps({
+    project:Object
+})
+</script>
+<template >
+ 
+        <div    :style="{ height: project.height }"
+ class="   relative group overflow-hidden rounded-3xl">
+                        <div :style='{backgroundImage: `url(${project.img})`}'
+                            class="w-full bg-cover bg-center rounded-3xl h-full group-hover:scale-105 transition transform duration-500 ease-in">
+                        </div>
+                        <div
+                            class=" w-full h-auto hover:bg-black/30 ease-in duration-300 transition-all rounded-2xl bg-linear-to-t from-black/90 from-0% via-black/40 via-25% to-transparent to-30% absolute inset-0 flex items-end justify-between py-3 px-6">
+                            <div class="transform translate-y-[65%] group-hover:translate-y-0 transition duration-300">
+                                <div class="flex  justify-between items-center ">
+                                    <h1 class="text-3xl font-projects font-semibold  text-white">{{ project.title }}</h1>
+                                    <span class="  text-black bg-white rounded-full p-2 ">
+                                        <ArrowUpRight
+                                            class="hover:scale-125 transition-all ease-in duration-200 cursor-pointer" />
+                                    </span>
+                                </div>
+                                <div class="opacity-0 group-hover:opacity-100 transition duration-300 mt-3">
+
+                                    <p class="font-body text-white text-sm py-2 text-center">Lorem ipsum dolor sit amet,
+                                        consectetur
+                                        adipisicing elit. Cumque magni alias ipsam?</p>
+                                    <!-- buttons -->
+                                    <div class="flex items-center justify-center  gap-3 py-2 w-full">
+                                        <button
+                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">view</button>
+                                        <button
+                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">contact</button>
+                                        <button
+                                            class="px-4 py-0.5 text-white border border-white rounded-full capitalize hover:bg-white/5 hover:scale-95 ease-in transition-all duration-100 cursor-pointer">learn</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+</template>
+
+<style scoped>
+    
+</style>
